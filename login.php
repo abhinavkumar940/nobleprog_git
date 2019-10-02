96c767e571403a2e18a8b159fb29167ba07ad3b9
Abhinav Kumar <abhinav@salesreporderform.com>
2019-10-02 06:27:57 +0530

    Login page completed

--- login.php
+++ login.php
@@ -1,8 +1,11 @@
-
+<!-- 
+  Sample HTML comment
+-->
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
+    <style href="styles.css"></style>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
@@ -25,15 +28,27 @@
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
       <label for="inputEmail" class="sr-only">Email address</label>
       <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+      <small id="passwordHelpBlock" class="form-text text-muted">
+        Should be a valid email and must be unique.
+      </small>
       <label for="inputPassword" class="sr-only">Password</label>
       <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <small id="passwordHelpBlock" class="form-text text-muted">
+        Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+      </small>
       <div class="checkbox mb-3">
         <label>
           <input type="checkbox" value="remember-me"> Remember me
         </label>
       </div>
       <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      <p class="mt-5 mb-3 text-muted">&copy; 2018-2020</p>
+      
     </form>
+    <footer>
+    <p class="mt-5 mb-3 text-muted">&copy; 2018-2020</p>
+    </footer>
+    <script>
+    // inline scripts
+    </script>
   </body>
 </html>
